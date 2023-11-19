6a95657e8d51e97d0eeafe3ef23ad34029e57f76
lianjz <1198315481@qq.com>
2023-11-19 15:35:02 +0800

    add orth

--- basicsr/losses/flare_loss.py
+++ basicsr/losses/flare_loss.py
@@ -120,3 +120,57 @@ class WeightedBCE(nn.Module):
         input = torch.clamp(input,min=1e-7,max=1-1e-7)
         bce = - self.class_weight[1] * target * torch.log(input) - (1 - target) * self.class_weight[0] * torch.log(1 - input)
         return torch.mean(bce)
+
+#matrix
+@LOSS_REGISTRY.register()
+class Orth_dist(nn.Module):
+    def __init__(self, stride, loss_weight=1.0):
+        super(Orth_dist, self).__init__()
+        self.stride=stride
+        self.loss_weight=loss_weight
+
+    def forward(self, mat):
+        mat = mat.reshape( (mat.shape[0], -1) )
+        if mat.shape[0] < mat.shape[1]:
+            mat = mat.permute(1,0)
+        return self.loss_weight * torch.norm( torch.t(mat)@mat - torch.eye(mat.shape[1]).cuda())
+
+#conv
+@LOSS_REGISTRY.register()
+class Orth_conv_dist(nn.Module):
+    def __init__(self, stride, loss_weight=1.0):
+        super(Orth_conv_dist, self).__init__()
+        self.stride = stride
+        self.loss_weight=loss_weight
+
+    def forward(self, kernel):
+       [o_c, i_c, w, h] = kernel.shape
+        assert (w == h),"Do not support rectangular kernel"
+        #half = np.floor(w/2)
+        assert self.stride < w,"Please use matrix orthgonality instead"
+        new_s = self.stride*(w-1) + w#np.int(2*(half+np.floor(half/stride))+1)
+        temp = torch.eye(new_s*new_s*i_c).reshape((new_s*new_s*i_c, i_c, new_s,new_s)).cuda()
+        out = (F.conv2d(temp, kernel, stride = self.stride)).reshape((new_s*new_s*i_c, -1))
+        Vmat = out[np.floor(new_s**2/2).astype(int)::new_s**2, :]
+        temp= np.zeros((i_c, i_c*new_s**2))
+        for i in range(temp.shape[0]):temp[i,np.floor(new_s**2/2).astype(int)+new_s**2*i]=1
+        return self.loss_weight * torch.norm( Vmat@torch.t(out) - torch.from_numpy(temp).float().cuda() )
+
+#deconv
+@LOSS_REGISTRY.register()
+class Orth_deconv_dist(nn.Module):
+    def __init__(self, stride, padding, loss_weight=1.0):
+        super(Orth_deconv_dist, self).__init__()
+        self.stride  = stride
+        self.padding = padding
+        self.loss_weight=loss_weight
+
+
+
+    def forward(self, kernel):
+        [o_c, i_c, w, h] = kernel.shape
+        output = torch.conv2d(kernel, kernel, stride=self.stride, padding=self.padding)
+        target = torch.zeros((o_c, o_c, output.shape[-2], output.shape[-1])).cuda()
+        ct = int(np.floor(output.shape[-1]/2))
+        target[:,:,ct,ct] = torch.eye(o_c).cuda()
+        return self.loss_weight * torch.norm( output - target )
